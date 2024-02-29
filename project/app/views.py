8607b3d1ad7d0253021e8ed5fe31b895be76ee59
Muteeb Bhat <muteebhat7@gmail.com>
2024-03-01 00:02:56 +0530

    -nasdaq ticker updated for close price

--- project/app/views.py
+++ project/app/views.py
@@ -66,6 +66,16 @@ def get_nasdaq_ticker(request, asset):
     print(asset)
     url = f'https://api.twelvedata.com/time_series?apikey=04ed091e9cac49aead575a1d1e1a3aa8&interval=1min&exchange=NASDAQ&outputsize=1&symbol={asset}'
     response = requests.get(url)
-    print(response)
-    return Response(response)
+    # print(type(response))
+    # return Response(response)
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        if 'values' in data and data['values']:
+            close_price = data['values'][0]['close']
+            return Response({'close_price': close_price})
+        else:
+            return Response({'error': 'No data available for the given asset symbol.'}, status=404)
+    else:
+        return Response({'error': 'Failed to fetch data from the API.'}, status=response.status_code)
     
