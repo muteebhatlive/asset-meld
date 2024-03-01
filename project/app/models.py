84e6efa94abbf40f2a9eb8e5f7c2a4f9f16106aa
Muteeb Bhat <muteebhat7@gmail.com>
2024-03-01 20:57:44 +0530

    - added total amount in crypto model

--- project/app/models.py
+++ project/app/models.py
@@ -8,10 +8,11 @@ class Crypto(models.Model):
     asset_name = models.CharField(max_length=255)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity_purchased = models.DecimalField(max_digits=10, decimal_places=5)
-    
-    @property
-    def total_dollar_amount(self):
-        return self.purchase_price * self.quantity_purchased
+    total_dollar_amount = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_dollar_amount = self.purchase_price * self.quantity_purchased
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return f"{self.user.username} - {self.asset_name}"
