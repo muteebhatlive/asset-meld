222f986289ddcae15225c4429ad467c03470fc8b
Muteeb Bhat <muteebhat7@gmail.com>
2024-03-03 23:19:41 +0530

    - Total Crypto Assets Model Added

--- project/app/models.py
+++ project/app/models.py
@@ -16,3 +16,17 @@ class Crypto(models.Model):
     
     def __str__(self):
         return f"{self.user.username} - {self.asset_name}"
+
+class CryptoTotal(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    asset_name = models.CharField(max_length=100)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_purchased = models.DecimalField(max_digits=10, decimal_places=5)
+    total_dollar_amount = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_dollar_amount = self.purchase_price * self.quantity_purchased
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f"{self.user.username}'s Crypto Total"
